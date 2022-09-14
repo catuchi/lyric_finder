ead2b231214b01ad0a658cdd70a396aa51a51fe9
Chibu <atuchibueze@gmail.com>
2022-09-13 20:53:52 -0400

    implement lyrics page

--- src/components/tracks/Lyrics.jsx
+++ src/components/tracks/Lyrics.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
 
 const Lyrics = () => {
   let { id } = useParams();
@@ -40,11 +41,46 @@ const Lyrics = () => {
     // fetchTrack();
   }, []);
 
-  return (
-    <div>
-      <h1>Lyrics</h1>
-    </div>
+  return track === undefined ||
+    lyrics === undefined ||
+    Object.keys(track).length === 0 ||
+    Object.keys(lyrics).length === 0 ? (
+    <Spinner />
+  ) : (
+    <>
+      <Link to={"/"} className="btn btn-dark btn-sm mb-4">
+        Go Back
+      </Link>
+      <div className="card">
+        <h5 className="card-header">
+          {track.track_name} by{" "}
+          <span className="text-secondary">{track.artist_name}</span>
+        </h5>
+        <div className="card-body">
+          <p className="cart-text">{lyrics.lyrics_body}</p>
+        </div>
+      </div>
+
+      <ul className="list-group mt-3">
+        <li className="list-group-item">
+          <strong>Album ID</strong>: {track.album_id}
+        </li>
+        <li className="list-group-item">
+          <strong>Song Genre</strong>:{" "}
+          {
+            track.primary_genres.music_genre_list[0].music_genre
+              .music_genre_name
+          }
+        </li>
+        <li className="list-group-item">
+          <strong>Explicit Words</strong>: {track.explicit === 0 ? "No" : "Yes"}
+        </li>
+      </ul>
+    </>
   );
+  // <div>
+  //   <h1>Lyrics</h1>
+  // </div>
 };
 
 export default Lyrics;
